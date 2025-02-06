1b7a1ab1a231d12bcffd4c0dd2c53f9e539bf526
wahyu agus arifin <itpohgero@gmail.com>
2025-02-06 17:57:22 +0700

    Update lokio formula to v1.0.1

--- Formula/lokio.rb
+++ Formula/lokio.rb
@@ -1,9 +1,9 @@
 class Lokio < Formula
   desc 'Lokio CLI tool'
   homepage 'https://github.com/any-source/lokio'
-  version '0.0.4'
-  url 'https://github.com/any-source/lokio/releases/download/v0.0.4/lokio-darwin-x64.tar.gz'
-  sha256 "b57bd2964e6314d454e7bfb64bb121f2abb5a6c9fc3a8223c873b1a96f296ff6"
+  version '1.0.1'
+  url 'https://github.com/any-source/lokio/releases/download/v1.0.1/lokio-darwin-x64.tar.gz'
+  sha256 "6e3ccf72d7ee6f1a530c88c4019311a4ea4b34ae382f66c9972cd9c3d08d8003"
 
   def install
     if Hardware::CPU.arm?
@@ -14,6 +14,6 @@ class Lokio < Formula
   end
 
   test do
-    assert_match "0.0.4", shell_output("\#{bin}/lokio --version")
+    assert_match "1.0.1", shell_output("\#{bin}/lokio --version")
   end
 end
